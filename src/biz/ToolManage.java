e3d38e32d8138d68be8f83404c526b94a5c839d8
lin2270707 <541510068@qq.com>
2017-08-29 16:58:21 +0800

    道具

--- src/biz/ToolManage.java
+++ src/biz/ToolManage.java
@@ -1,16 +1,42 @@
 package biz;
 
+import java.util.List;
+
+import po.DeadPeople;
+import po.NormalPeople;
+import po.People;
+import po.Tool;
 
 //道具管理接口
 public interface ToolManage {
 	
 	public boolean isWeopon(int ttype);
+	//判断是否有武器，若有武器则返回true,若没有武器则返回false
 	
-	public int weoponDamage(int attackValue);//刀、枪
+	public int addWeoponDamage(int attackValue);
+	//计算武器的伤害,返回值为int型
+	
 	
 	public boolean isBomb(int ttype);
+	//判断是否有炸弹，若有炸弹则返回true,若没有炸弹则返回false
 	
+	public void bombFuction(List<People> plist,
+			NormalPeople np,DeadPeople dp);
+	//传入发生战斗的双方的实例,销毁实例，并从list中删除
 	
+	public void destoryBomb(NormalPeople np,List<Tool> tlist,
+			int tid);
+	//传入该武器的tid并在全局tlist和人物携带tlist中销毁
 	
+	public boolean isShoes(int ttype);
+	//判断是否有鞋子,有则不变返回true，没有则返回false
+	
+	public boolean escapeShoes();//逃跑鞋方法
+	
+	public boolean speedShoes();//加速鞋方法
+	
+	public boolean isDoping();//是否有兴奋剂
+	
+	public People findPid(int pid,List<People> plist);
 	
 }
--- src/biz/ToolManageImpl.java
+++ src/biz/ToolManageImpl.java
@@ -1,7 +1,49 @@
 package biz;
 
 //道具管理接口实现
-public class ToolManageImpl {
+public class ToolManageImpl implements ToolManage{
+
+	@Override
+	public boolean isWeopon(int ttype) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int weoponDamage(int attackValue) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isBomb(int ttype) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isShoes(int type) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean escapeShoes() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean speedShoes() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isDoping() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 	
 
