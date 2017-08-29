234c521fa8d8ded08b6f3d7c649760fb2fe327c1
lin2270707 <541510068@qq.com>
2017-08-29 23:29:14 +0800

    有地图的加同步成功的今晚最终版

--- src/biz/LandEventImpl.java
+++ src/biz/LandEventImpl.java
@@ -1,7 +1,20 @@
 package biz;
 
+import po.NormalPeople;
 
 //土地事件接口实现
 public class LandEventImpl implements LandEvent{
+
+	@Override
+	public int findLandByPeople(NormalPeople np) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int isShelter(int ltype, NormalPeople np) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
