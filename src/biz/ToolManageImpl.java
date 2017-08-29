f192d6fa69969be7efc099a561e219e8cb1d5f6f
lin2270707 <541510068@qq.com>
2017-08-29 20:28:34 +0800

    。。。
    。。。

--- src/biz/ToolManageImpl.java
+++ src/biz/ToolManageImpl.java
@@ -1,5 +1,12 @@
 package biz;
 
+import java.util.List;
+
+import po.DeadPeople;
+import po.NormalPeople;
+import po.People;
+import po.Tool;
+
 //道具管理接口实现
 public class ToolManageImpl implements ToolManage{
 
@@ -7,12 +14,6 @@ public class ToolManageImpl implements ToolManage{
 	public boolean isWeopon(int ttype) {
 		// TODO Auto-generated method stub
 		return false;
-	}
-
-	@Override
-	public int weoponDamage(int attackValue) {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 	@Override
@@ -43,6 +44,30 @@ public class ToolManageImpl implements ToolManage{
 	public boolean isDoping() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public int addWeoponDamage(int attackValue) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void bombFuction(List<People> plist, NormalPeople np, DeadPeople dp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void destoryBomb(NormalPeople np, List<Tool> tlist, int tid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public People findPid(int pid, List<People> plist) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
