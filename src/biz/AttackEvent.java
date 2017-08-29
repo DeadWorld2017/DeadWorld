e77e43c150f50af795be2ef9ff4bd83eb8ba16f5
373628977@qq.com <373628977@qq.com>
2017-08-29 17:00:09 +0800

    Land
    接口改变

--- src/biz/AttackEvent.java
+++ src/biz/AttackEvent.java
@@ -2,5 +2,6 @@ package biz;
 
 //计算攻击力接口
 public interface AttackEvent {
-
+	//计算正常人类的攻击力
+	
 }
--- src/biz/LandEvent.java
+++ src/biz/LandEvent.java
@@ -1,7 +1,29 @@
 package biz;
 
+import po.Land;
+import po.NormalPeople;
 
-//土地事件接口
+/*地形类,存放对应得地图坐标和地形类型
+ * 
+ * 类型1：庇护所
+ * 类型2：辐射地
+ * 类型3：沼泽地
+ * 类型4：子母河
+ * 类型5：死亡陷阱
+ * 类型6：困阵	
+ * 
+ */
+//地形事件接口
 public interface LandEvent {
+	
+	//通过传进人类实例，通过坐标寻找对应的地形，返回对应的land的类型编号（1-6），即ltype
+	public int findLandByPeople(NormalPeople np);
+
+	//战斗前判断,若是则根据事件返回相
+	public int isShelter(int ltype,NormalPeople np);
+	
+	//战斗时判断
+	
+	//战斗后判断
 
 }
