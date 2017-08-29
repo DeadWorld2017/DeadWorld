e01329ebb8e9f8038b6ede6ab90775889a0ada2b
lin2270707 <541510068@qq.com>
2017-08-29 15:58:21 +0800

    再传一次

--- src/biz/ManageImpl.java
+++ src/biz/ManageImpl.java
@@ -3,5 +3,5 @@ package biz;
 
 //管理接口实现
 public class ManageImpl implements Manage{
-
+	
 }
--- src/biz/ToolManage.java
+++ src/biz/ToolManage.java
@@ -4,8 +4,13 @@ package biz;
 //道具管理接口
 public interface ToolManage {
 	
+	public boolean isWeopon(int ttype);
 	
 	public int weoponDamage(int attackValue);//刀、枪
+	
+	public boolean isBomb(int ttype);
+	
+	
 	
 	
 }
